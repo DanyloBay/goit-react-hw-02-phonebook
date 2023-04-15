f55ff5085e49f2b26aa882d3015b9e0280ef5353
Danylo Bay <danylobay@gmail.com>
2023-04-15 11:43:38 +0200

    optimize

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -2,16 +2,12 @@ import PropTypes from 'prop-types';
 import css from './ContactList.module.css';
 
 export const ContactList = ({ contacts, onClick }) => {
-  const handleDeleteContact = id => {
-    onClick(id);
-  };
-
   return (
     <ul className={css.contacts}>
       {contacts.map(({ id, name, number }) => (
         <li key={id} className={css.contactsItem}>
           {name}: {number}
-          <button type="button" onClick={() => handleDeleteContact(id)}>
+          <button type="button" onClick={() => onClick(id)}>
             Delete
           </button>
         </li>
